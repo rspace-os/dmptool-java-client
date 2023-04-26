3d2a37099f1b1cea205733c85413e4224b1739bd
Robert Lamacraft <robert.lamacraft@researchspace.com>
2023-04-26 10:54:49 +0100

    Store dmproadmap_links and get id when needed

--- src/main/java/com/researchspace/dmptool/model/DMPToolDMP.java
+++ src/main/java/com/researchspace/dmptool/model/DMPToolDMP.java
@@ -18,45 +18,20 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 @JsonTypeName(value = "dmp")
 public class DMPToolDMP extends DMP {
 
-	private Long id;
+	@JsonProperty("dmproadmap_links")
+	private Map<String, String> links;
 
-  @JsonProperty("dmproadmap_links")
-  private Map<String, String> links;
-
-  @JsonProperty("id")
-  public void setId() {
-		// Map<String,String> links = (Map<String,String>) dmp.get("dmproadmap_links");
-		String getUrlString = links.get("get");
+	/*
+	 * This is... unfortunate. Would be much better for us to be using the
+	 * proper dmp_id property as a unique identifier, rather than the
+	 * internal implementation detail used by DMPTool. However, it is now
+	 * used as a database key (UserDMP) so removing this without breaking
+	 * backwards compatibility will be difficult.
+	 */
+	public Long getId() {
+		String getUrlString = this.links.get("get");
 		String idString = getUrlString.substring(getUrlString.lastIndexOf("/") + 1);
-
-    this.id = Long.parseLong(idString); 
-	}
-  // we want to use @JsonGetter to define a method that just gets the id
-
-	public DMPToolDMP(Long id, String title, String description) {
-		super();
-		this.setTitle(title);
-		this.setDescription(description);
-		this.id = id;
+		return Long.parseLong(idString);
 	}
 
-	// can I avoid this? It means that the other properties of DMP are not getting parsed
-  // is there a way of delegating all other properties to the standard parsing but still be able to parse the id in this whacky way?
-  // Is this class really being passed { dmp: { ... } } ??
-	// @SuppressWarnings("unchecked")
-	// @JsonProperty("dmp")
-	// private void foo(Map<String,Object> dmp) {
-	// 	this.setTitle((String) dmp.get("title"));
-	// 	this.setDescription(Optional.of((String) dmp.get("description")));
-
-	// 	Map<String,String> links = (Map<String,String>) dmp.get("dmproadmap_links");
-	// 	String getUrlString = links.get("get");
-	// 	String idString = getUrlString.substring(getUrlString.lastIndexOf("/") + 1);
-
-	// 	try {
-	// 	 this.id = Long.parseLong(idString); 
-	// 	} catch (NumberFormatException e) {
-	// 	 // ignore
-	// 	}
-	// }
 }
